d43758631e6858c6cfc8c715eb6292c6e365745d
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-05 03:54:17 +0300

    Ilk MVC uygulamamiz

---
+++ MVCOrnek1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCOrnek1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            DateTime zaman = DateTime.Now;
+            ViewBag.AnlikZaman = zaman; //istedigimiz turde degiskeni atamamizi saglar.
+
+            return View();
+        }
+    }
+}
